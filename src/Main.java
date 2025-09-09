e22b2123f6c43266c1807434fe5cf013babadf41
uladzislau_maiseyeu <vladislavmoiseev9@gmail.com>
2025-09-09 17:19:58 +0300

    added object counter

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import Statick.ObjectCounter;
+
 public class Main {
     public static void main(String[] args) {
 
+        ObjectCounter objectCounter1 = new ObjectCounter();
+        ObjectCounter objectCounter2 = new ObjectCounter();
+        ObjectCounter objectCounter3 = new ObjectCounter();
+        ObjectCounter objectCounter4 = new ObjectCounter();
+
+        System.out.println(ObjectCounter.getCount());
     }
 }
