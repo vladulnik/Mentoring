0dd7d275636e42a5c231e837ccb21ec54ee4f0a2
uladzislau_maiseyeu <vladislavmoiseev9@gmail.com>
2025-09-11 14:23:16 +0300

    added new class to practice with final

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,21 @@ import problemSolvingClasses.animals.Animal;
 import problemSolvingClasses.animals.Bird;
 import problemSolvingClasses.animals.Cat;
 import problemSolvingClasses.animals.Dog;
+import problemSolvingClasses.workWithFinal.Book;
+import problemSolvingClasses.workWithFinal.Item;
+import problemSolvingClasses.workWithFinal.Library;
+import problemSolvingClasses.workWithFinal.ShoppingCart;
 import utils.AnimalUtils;
+
+import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
+        Library library = new Library(2);
+        library.addBook(new Book("Война и мир", "Лев Толстой"));
+        library.addBook(new Book("Преступление и наказание", "Ф.М. Достоевский"));
+        library.printBooks();
 
-        Animal[] animal = {
-                new Dog(),
-                new Cat(),
-                new Bird(),
-                new Dog(),
-                new Cat(),
-                new Bird(),
-                new Bird(),
-                new Bird()
-        };
-
-        AnimalUtils.countAnimals(animal);
+        library.addBook(new Book("Мастер и Маргарита", "М.А. Булгаков")); // тут должно появиться сообщение, что библиотека заполнена
     }
 }
---
+++ src/problemSolvingClasses/workWithFinal/ShoppingCart.java
@@ -0,0 +1,26 @@
+package problemSolvingClasses.workWithFinal;
+
+public class ShoppingCart {
+
+    private final Item[] items;
+    private int count = 0;
+
+
+    public ShoppingCart(int capacity) {
+        this.items = new Item[capacity];
+    }
+
+    public void addItem(Item item) {
+        if(count < items.length) {
+            items[count++] = item;
+        } else {
+            System.out.println("No more space");
+        }
+    }
+
+    public void printCart() {
+        for(int i = 0; i < count; i++) {
+            System.out.println(items[i].getName());
+        }
+    }
+}
